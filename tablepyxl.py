bcb072f59eac679f688904bc9f40b5bf7d78de5e
martsberger <bmarts@procuredhealth.com>
2015-06-21 09:59:59 -0500

    Support a few styles, bold and text-align right.

--- tablepyxl.py
+++ tablepyxl.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as BS
 from openpyxl import Workbook
-from openpyxl.cell import Cell
+from openpyxl.styles import Font, Alignment, Style
+from premailer import transform
 
 
 def get_tables(doc):
@@ -11,6 +12,11 @@ def get_tables(doc):
     return soup.find_all('table')
 
 
+def get_Tables(doc):
+    soup = BS(doc)
+    return [Table(table) for table in soup.find_all('table')]
+
+
 def write_rows(worksheet, elem, row, column=1):
     """
     Writes every tr child element of elem to a row in the worksheet
@@ -18,11 +24,17 @@ def write_rows(worksheet, elem, row, column=1):
 
     returns the next row after all rows are written
     """
-    for table_row in elem.find_all('tr'):
-        for cell in table_row.find_all('th') + table_row.find_all('td'):
-            worksheet.cell(row=row, column=column).value = cell.text
+    initial_column = column
+    for table_row in elem.rows:
+        for table_cell in table_row.cells:
+            cell = worksheet.cell(row=row, column=column)
+            cell.value = table_cell.element.text
+            style = table_cell.style()
+            cell.font = style.font
+            cell.alignment = style.alignment
             column += 1
         row += 1
+        column = initial_column
     return row
 
 
@@ -31,7 +43,7 @@ def table_to_sheet(table, wb):
     Takes a table and workbook and writes the table to a new sheet.
     The sheet title will be the same as the table attribute name.
     """
-    ws = wb.create_sheet(title=table.get('name', None))
+    ws = wb.create_sheet(title=table.element.get('name', None))
     insert_table(table, ws, 1, 1)
 
 
@@ -42,7 +54,8 @@ def document_to_xl(doc, filename):
     """
     wb = Workbook()
     wb.remove_sheet(wb.active)
-    tables = get_tables(doc)
+    inline_styles_doc = transform(doc)
+    tables = get_Tables(inline_styles_doc)
 
     for table in tables:
         table_to_sheet(table, wb)
@@ -51,8 +64,8 @@ def document_to_xl(doc, filename):
 
 
 def insert_table(table, worksheet, column, row):
-    row = write_rows(worksheet, table.thead, row, column)
-    row = write_rows(worksheet, table.tbody, row, column)
+    row = write_rows(worksheet, table.head, row, column)
+    row = write_rows(worksheet, table.body, row, column)
 
 
 def insert_table_at_cell(table, cell):
@@ -62,3 +75,82 @@ def insert_table_at_cell(table, cell):
     ws = cell.parent
     column, row = cell.column, cell.row
     insert_table(table, ws, column, row)
+
+
+def style_string_to_dict(style):
+    def clean_split(string, delim):
+        return (s.strip() for s in string.split(delim))
+    styles = [clean_split(s, ":") for s in style.split(";") if ":" in s]
+    return dict(styles)
+
+
+def style_dict_to_Style(style):
+    """
+    change css style to openpyxl Style
+    """
+    # Font
+    font_kwargs = {'bold': style.get('font-weight') == 'bold'}
+    font = Font(**font_kwargs)
+
+    # Alignment
+    alignment_kwargs = {'horizontal': style.get('text-align', 'general')}
+    alignment = Alignment(**alignment_kwargs)
+
+    pyxl_style = Style(font=font, alignment=alignment)
+
+    return pyxl_style
+
+
+class Element(object):
+    def __init__(self, element, parent=None):
+        self.element = element
+        self.parent = parent
+        self.style_dict = style_string_to_dict(element.get('style', ''))
+        self._style_cache = None
+
+    def get_style(self, key):
+        if key in self.style_dict:
+            return self.style_dict[key]
+        if self.parent:
+            return self.parent.get_style(key)
+        return None
+
+    def style(self):
+        """
+        Turn the css styles for this element into an openpyxl Style
+        """
+        if not self._style_cache:
+            self._style_cache = style_dict_to_Style(self.style_dict)
+        return self._style_cache
+
+
+class Table(Element):
+    def __init__(self, table):
+        """
+        takes an html table object (from BeautifulSoup)
+        """
+        super(Table, self).__init__(table)
+        self.head = TableHead(table.thead, parent=self)
+        self.body = TableBody(table.tbody, parent=self)
+
+
+class TableHead(Element):
+    def __init__(self, head, parent=None):
+        super(TableHead, self).__init__(head, parent=parent)
+        self.rows = [TableRow(tr, parent=self) for tr in head.find_all('tr')]
+
+
+class TableBody(Element):
+    def __init__(self, body, parent=None):
+        super(TableBody, self).__init__(body, parent=parent)
+        self.rows = [TableRow(tr, parent=self) for tr in body.find_all('tr')]
+
+
+class TableRow(Element):
+    def __init__(self, tr, parent=None):
+        super(TableRow, self).__init__(tr, parent=parent)
+        self.cells = [TableCell(cell, parent=self) for cell in tr.find_all('th') + tr.find_all('td')]
+
+
+class TableCell(Element):
+    pass
