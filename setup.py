641b76300d0851a299b9b90c7558513033092c0d
martsberger <bmarts@procuredhealth.com>
2015-07-10 14:44:59 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tablepyxl',
+    version='0.1',
+    description='Generate Excel documents from html tables',
+    url='https://github.com/martsberger/tablepyxl',
+    author='Brad Martsberger',
+    author_email='bmarts@procuredhealth.com',
+    packages=find_packages(),
+    install_requires=['openpyxl', 'beautifulsoup4', 'premailer']
+)
