722bf6fb9856360f9472d052398ca2d5f8d06895
martsberger <bmarts@procuredhealth.com>
2017-02-22 22:52:06 -0500

    Remove py35 from tox because it didn't find a python 3.5 interpreter.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.3.6',
     description='Generate Excel documents from html tables',
     url='https://github.com/martsberger/tablepyxl',
-    download_url='https://github.com/martsberger/tablepyxl/archive/0.3.5.tar.gz',
+    download_url='https://github.com/martsberger/tablepyxl/archive/0.3.6.tar.gz',
     author='Brad Martsberger',
     author_email='bmarts@procuredhealth.com',
     license='MIT',
@@ -13,8 +13,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4'
     ],
     packages=find_packages(),
     install_requires=['openpyxl', 'beautifulsoup4', 'premailer', 'requests', 'lxml']
