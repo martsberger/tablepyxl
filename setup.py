202fca9661c309a21cc96357fb66ab334b5a0ac6
martsberger <bmarts@procuredhealth.com>
2017-02-22 21:37:24 -0500

    Add python versions to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablepyxl',
-    version='0.3.5',
+    version='0.3.6',
     description='Generate Excel documents from html tables',
     url='https://github.com/martsberger/tablepyxl',
     download_url='https://github.com/martsberger/tablepyxl/archive/0.3.5.tar.gz',
     author='Brad Martsberger',
     author_email='bmarts@procuredhealth.com',
     license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=find_packages(),
     install_requires=['openpyxl', 'beautifulsoup4', 'premailer', 'requests', 'lxml']
 )
