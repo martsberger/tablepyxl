42ca6074e1a0314b018bf6df007ac397fcb97d0a
martsberger <bmarts@procuredhealth.com>
2017-09-25 15:42:34 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablepyxl',
-    version='0.4.3',
+    version='0.4.4',
     description='Generate Excel documents from html tables',
     url='https://github.com/martsberger/tablepyxl',
-    download_url='https://github.com/martsberger/tablepyxl/archive/0.4.3.tar.gz',
+    download_url='https://github.com/martsberger/tablepyxl/archive/0.4.4.tar.gz',
     author='Brad Martsberger, Asma Mehjabeen, Brian Davis',
     author_email='bmarts@procuredhealth.com',
     license='MIT',
