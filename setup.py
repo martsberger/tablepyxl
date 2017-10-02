8c0581d264efb576f45d683ee24af2021eb85616
martsberger <bmarts@procuredhealth.com>
2017-10-01 23:05:59 -0400

    Update python version in setup.py since the tests are passing in those versions.

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
     ],
     packages=find_packages(),
     install_requires=['openpyxl', 'premailer', 'requests', 'lxml']
