f961e92d8a621dba9faafa93b2e94cd776453ee8
martsberger <bmarts@lumere.com>
2019-04-01 23:06:47 -0400

    Bump version up to 0.6.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablepyxl',
-    version='0.5',
+    version='0.6.0',
     description='Generate Excel documents from html tables',
     url='https://github.com/martsberger/tablepyxl',
-    download_url='https://github.com/martsberger/tablepyxl/archive/0.5.tar.gz',
+    download_url='https://github.com/martsberger/tablepyxl/archive/0.6.0.tar.gz',
     author='Brad Martsberger, Asma Mehjabeen, Brian Davis',
-    author_email='bmarts@procuredhealth.com',
+    author_email='bmarts@lumere.com',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 2',
@@ -15,7 +15,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     packages=find_packages(),
     install_requires=['openpyxl', 'premailer', 'requests', 'lxml']
